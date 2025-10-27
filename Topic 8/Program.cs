fc6c963a63830cb6091766fa5ebe7eb631a2ac36
Alexander Ronan <339828832@GoTVDSB.ca>
2025-10-27 08:46:23 -0400

    Finished

--- Topic 8/Program.cs
+++ Topic 8/Program.cs
@@ -45,7 +45,7 @@ namespace Topic_8
 
         public static void Numbers()
         {
-            int randMax = 0, randMin = 1, loopAmount, guessNum, equalToGuess = 0;
+            int randMax = 0, randMin = 1, loopAmount, guessNum, equalToGuess = 0, replaceNum;
 
             Random generator = new Random();
 
@@ -63,6 +63,7 @@ namespace Topic_8
 
             while (randMin > randMax)
             {
+                randMin = 1; randMax = 0;
                 while (!int.TryParse(Console.ReadLine(), out randMin))
                 {
                     Console.WriteLine("Enter a minimum value.");
@@ -77,6 +78,7 @@ namespace Topic_8
                     int.TryParse(Console.ReadLine(), out randMax);
                 }
             }
+
             for (int i = 0; i < loopAmount; i++)
             {
                 RandomNumbers.Add(generator.Next(randMin, randMax));
@@ -85,19 +87,88 @@ namespace Topic_8
             for (int i = 0; i < loopAmount; i++) // Number 1.
                 Console.WriteLine(RandomNumbers[i] + ", ");
 
+            // 2. Prompt the user for a number and count the number of times that number appears in the List.
+
             Console.WriteLine("Pick a number between " + randMin + " and " + randMax + ".");
+            int.TryParse(Console.ReadLine(), out guessNum);
+            while (guessNum > randMax || guessNum < randMin)
+            {
                 Console.WriteLine("Pick a number between " + randMin + " and " + randMax + ".");
                 int.TryParse(Console.ReadLine(), out guessNum);
-
+            }
             for (int i = 0; i < RandomNumbers.Count; i++)
             {
                 if (RandomNumbers[i] == guessNum)
-                    equalToGuess = equalToGuess + 1;
+                   equalToGuess = equalToGuess + 1;
 
             }
 
-            Console.WriteLine(RandomNumbers[i]);
+            Console.WriteLine("There are " + equalToGuess + " " + guessNum);
 
+            // 3. Prompt the user for a number and replace all occurrences of that number with zero.
+            Console.WriteLine("Pick another number between " + randMin + " and " + randMax + ".");
+            int.TryParse(Console.ReadLine(), out replaceNum);
+            while (replaceNum > randMax || replaceNum < randMin)
+            {
+                Console.WriteLine("Pick another number between " + randMin + " and " + randMax + ".");
+                int.TryParse(Console.ReadLine(), out replaceNum);
+            }
+            for (int i = 0; i < RandomNumbers.Count; i++)
+            {
+                if (RandomNumbers[i] == replaceNum)
+                    RandomNumbers[i] = 0;
+            }
+
+            // 4. Print the List to the screen on one line.
+            for (int i = 0; i < loopAmount; i++)
+                Console.Write(RandomNumbers[i] + ", ");
+
+            // 5. Using a loop, reset each value in the List with zeros.
+            Console.WriteLine();
+            Console.WriteLine("Resetting All Numbers");
+            
+            for (int i = 0; i < loopAmount; i++)
+                // 6. Print the List to the screen on one line.
+                Console.Write(RandomNumbers[i] = 0);
+
+            // 7. Use a loop to fill the List again with new random numbers.
+
+            Console.WriteLine();
+            Console.WriteLine("Let's fill the list with new numbers!");
+
+            Console.WriteLine("Enter a minimum value.");
+            randMin = 1; randMax = 0;
+
+            while (randMin > randMax)
+            {
+                randMin = 1; randMax = 0;
+                while (!int.TryParse(Console.ReadLine(), out randMin))
+                {
+                    Console.WriteLine("Enter a minimum value.");
+                    int.TryParse(Console.ReadLine(), out randMin);
+                }
+
+                Console.WriteLine("Enter a maximum value.");
+
+                while (!int.TryParse(Console.ReadLine(), out randMax))
+                {
+                    Console.WriteLine("Enter a maximum value.");
+                    int.TryParse(Console.ReadLine(), out randMax);
+                }
+
+
+            }
+          
+            RandomNumbers.Clear();
+
+            for (int i = 0; i < loopAmount; i++)
+            {
+                RandomNumbers.Add(generator.Next(randMin, randMax));
+            }
+
+            // 8. Print the List to the screen on one line.
+            for (int i = 0; i < loopAmount; i++)
+                Console.Write(RandomNumbers[i] + ", ");
         }
     }
 }
